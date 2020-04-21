da2eccad28f3980d665a2e3cac60b7d9c5b9e890
Samuel Walker <sewalker191@gmail.com>
2020-04-21 11:49:36 -0700

    Add user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])  
+    end
+
+    def require_logged_in
+        render json: ["Must be logged in to use this feature"], status: 422 unless logged_in?
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token! if logged_in?
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
