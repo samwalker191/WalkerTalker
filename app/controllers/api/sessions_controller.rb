536be8b1693c77d9f6f030f5f6a87ab104e5139d
Samuel Walker <sewalker191@gmail.com>
2020-04-21 12:14:37 -0700

    Add backend user auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            login(@user)
+            render 'api/users/show'
+        else
+            render json: ["Invalid Credentials"], status: 422
+        end
+    end
+
+    def destroy
+        if current_user
+            @current_user = current_user
+            logout!
+            render json: {}
+        else
+            render json: ["No urrent user"], status: 422
+        end
+    end
+end
