23e27bb758404c3c629bce85e7bb050961615944
Samuel Walker <sewalker191@gmail.com>
2020-04-29 19:21:50 -0700

    Add css module compatability

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,28 @@ module.exports = {
                         presets: ['@babel/env', '@babel/react']
                     }
                 },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true
+                        }
+                    }
+                ],
+                include: /\.module\.css$/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                exclude: /\.module\.css$/
             }
         ]
     },
